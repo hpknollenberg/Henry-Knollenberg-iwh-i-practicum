fb40ac896cac6ef59034231109c9e83b65fef197
Henry Knollenberg <henry@heroninnovators.com>
2024-11-14 17:19:39 -0500

    Get request update-cobj

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,10 +9,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
+app.get('/update-cobj', async (req, res) => {
+    const pets = 'https://api.hubspot.com/crm/v3/objects/pets'
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(pets, { headers })
+        const data = resp.data.results
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data})
+    } catch (error) {
+        console.error(error)
+    }
+})
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
@@ -22,7 +36,7 @@ const PRIVATE_APP_ACCESS = '';
 
 // * Code for Route 3 goes here
 
-/** 
+/* * 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
 * * App.get sample
